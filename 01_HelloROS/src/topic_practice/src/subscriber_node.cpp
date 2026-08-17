fa04f4339499344dfcb7e35b98ac270110208c0c
xhy <3560037741@qq.com>
2026-08-17 20:49:44 +0800

    第二个任务

---
+++ 01_HelloROS/src/topic_practice/src/subscriber_node.cpp
@@ -0,0 +1,33 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class SubscriberNode : public rclcpp::Node{
+  private:
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+public:
+    SubscriberNode() : Node("subscriber_node")
+    {
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
+            "chatter",
+            10,
+            std::bind(
+              &SubscriberNode::message_callback, 
+              this,
+              std::placeholders::_1)
+        );
+    }
+
+    void message_callback(const std_msgs::msg::String::SharedPtr msg){
+        RCLCPP_INFO(this->get_logger(), 
+        "订阅: %s",
+         msg->data.c_str());
+    } 
+};
+
+int main(int argc,char* argv[]){
+    rclcpp::init(argc,argv);
+    rclcpp::spin(std::make_shared<SubscriberNode>());
+    rclcpp::shutdown();
+    return 0;
+
+}
